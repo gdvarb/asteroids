6a07c967adf16d299a45126ca104b58af810629e
Georgi Varbanov <gvarbanov@usf.edu>
2025-07-11 10:46:54 -0400

    Creat constants

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pygame
-
+from constants import *
 def main():
     print("Starting Asteroids!")
-
-
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
 if __name__ == "__main__":
     main()
