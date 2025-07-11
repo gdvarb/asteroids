dc563a2233eb797612c1149fd77e06ef300b779a
Georgi Varbanov <gvarbanov@usf.edu>
2025-07-11 13:22:50 -0400

    Defined circle class

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import pygame
 from constants import *
 def main():
-   # print("Starting Asteroids!")
-   # print("Screen width:", SCREEN_WIDTH)
-   # print("Screen height:", SCREEN_HEIGHT)
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     loop = True
+    clock = pygame.time.Clock()
+    dt = 0
     while loop:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
-        pygame.display.flip()
+        pygame.display.flip()    
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
         main()
