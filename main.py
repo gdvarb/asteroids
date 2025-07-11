66271d506465e42b731107f7de1c5dfbe38b4996
Georgi Varbanov <gvarbanov@usf.edu>
2025-07-11 12:13:14 -0400

    Add gui window

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import pygame
 from constants import *
 def main():
-    print("Starting Asteroids!")
-    print("Screen width:", SCREEN_WIDTH)
-    print("Screen height:", SCREEN_HEIGHT)
+   # print("Starting Asteroids!")
+   # print("Screen width:", SCREEN_WIDTH)
+   # print("Screen height:", SCREEN_HEIGHT)
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    loop = True
+    while loop:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        pygame.display.flip()
+
 if __name__ == "__main__":
-    main()
+        main()
+    
