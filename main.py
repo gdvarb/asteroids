3d0b6213af1725854273fe73fb52db62f71ca661
Georgi Varbanov <gvarbanov@usf.edu>
2025-07-13 11:02:46 -0400

    Add player obj groups

--- main.py
+++ main.py
@@ -6,22 +6,32 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     loop = True
     clock = pygame.time.Clock()
-    dt = 0
     x_coordinate = SCREEN_WIDTH / 2
     y_coordinate = SCREEN_HEIGHT / 2
+
+    # creates containers
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    # set containers that the object will automatically join
+    Player.containers = (updatable, drawable)
+    # instantiation of the Player obj automatically adds it to the containers
     player = Player(x_coordinate, y_coordinate)
 
+    dt = 0
     while loop:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        dt = clock.tick(60) / 1000
+        updatable.update(dt)
+
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()    
 
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
         main()
