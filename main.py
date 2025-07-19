f45405fd4c4793845cca6cf9713aaebd431cccd0
Georgi Varbanov <gvarbanov@usf.edu>
2025-07-19 11:38:28 -0400

    Add asteroid and shot collision

--- main.py
+++ main.py
@@ -41,6 +41,12 @@ def main():
                 print("Game Over !")
                 sys.exit()
 
+        for asteroid in asteroids:
+            for shot in shots:
+                if shot.collision(asteroid):
+                    asteroid.kill()
+                    shot.kill()
+                    
         screen.fill("black")
         for obj in drawable:
             obj.draw(screen)
